b163900e6decc1b674d72982aa136f07325f33dd
David CM <dhf360@gmail.com>
2019-10-12 06:48:08 -0600

    Updates

---
+++ precache-manifest.6963d5577a7c27880bb63e4457639653.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45c3a36085fca62473916687447abf1d",
+    "url": "/Accessible-remote-board./index.html"
+  },
+  {
+    "revision": "0114ba2751723967633e",
+    "url": "/Accessible-remote-board./static/css/main.9e616118.chunk.css"
+  },
+  {
+    "revision": "74b47d40e2d22b30398e",
+    "url": "/Accessible-remote-board./static/js/2.438b196b.chunk.js"
+  },
+  {
+    "revision": "0114ba2751723967633e",
+    "url": "/Accessible-remote-board./static/js/main.458c8028.chunk.js"
+  },
+  {
+    "revision": "faf84afdfd0ea7478ae5",
+    "url": "/Accessible-remote-board./static/js/runtime-main.20d2c9e3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Accessible-remote-board./static/media/logo.25bf045c.svg"
+  }
+]);
